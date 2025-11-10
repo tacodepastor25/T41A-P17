89832ec87378d76c960582df5e5189c75435dafd
Jesús Alberto Revilla Silva <jesus_revilla@yahoo.com>
2025-11-10 11:42:22 -0600

    Update test for user name from 'Pedrito' to 'Ana'

--- tests/test_usuarios.py
+++ tests/test_usuarios.py
@@ -17,7 +17,8 @@ def run_query(query):
 
 def test_nombre_ana():
     result = run_query("SELECT data->>'nombre' FROM usuarios WHERE id = 1;")
-    assert result[0][0] == "Pedrito"
+    print(f'resultado del query: {result}')
+    assert result[0][0] == "Ana"
 
 def test_usuario_activo():
     result = run_query("SELECT data->>'activo' FROM usuarios WHERE id = 1;")
